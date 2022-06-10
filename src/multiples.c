10b88f93313a28c0b4edf3f22a1e92fb2bb8959c
Jack DeVries <jdevries3133@gmail.com>
2022-06-10 17:03:57 -0400

    updates to multiples.c

--- src/multiples.c
+++ src/multiples.c
@@ -15,14 +15,6 @@ void parse_args(struct Arguments *args, char *input[], int num_args) {
     bool found_base = false;
     bool found_count = false;
 
-    // simplest usage with unnamed positional args
-    if (num_args == 3) {
-        args->base = atoi(input[1]);
-        args->count = atoi(input[2]);
-        args->valid = true;
-        return;
-    }
-
     // start i at 1 and stop when there is no more args ahead, because we
     // are looking for flags, then grabbing the next arg.
     for (int i = 1; i + 1 < num_args; i++) {
@@ -49,7 +41,7 @@ void print_multiples(int *base, int *num) {
 
 void print_help() {
     printf("USAGE\n\n");
-    printf("\tmultiples (--base)? [number] (--count)? [number]\n\n");
+    printf("\tmultiples --base [number] --count [number]\n\n");
     printf("Base is the number to find the multiples of. Count is the number\n");
     printf("of multiples to be printed.\n");
 }
